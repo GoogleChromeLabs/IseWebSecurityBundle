9aead82d785d5287b438a012396350f8bb5e2d61
Matthew Henry <henrym2@tcd.ie>
2020-07-17 12:48:03 +0100

    Added fetch metadata Policy Added Policy interface for fetch metadata Added Default Policy Modified Request Subscriber to use new policy interface Created new tests for Fetch Metadata policy and Subscriber Updated Config to include new configuration Updated service configuration to include new fetch metadata policy service Updated configuration to allow for custom policy to be assigned Updated config to include new endpoint exemptions

---
+++ Policies/FetchMetadataPolicyInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ise\WebSecurityBundle\Policies;
+
+use Symfony\Component\HttpFoundation\Request;
+
+interface FetchMetadataPolicyInterface {
+    /**
+     * Apply Policy interface. This Function should process a Symfony HTTP Kernel request, apply Fetch a Fetch Metadata policy
+     * and return TRUE if the request passes the policy and should be accepted, or FALSE if it does not pass the policy.  
+     *
+     * @param Request $request A symfony HTTP kernel request
+     * @return boolean 
+     */
+    public function applyPolicy(Request $request): bool;
+}
