c883228d5b160480322198d587d0c8c1cc07cde3
Matthew Henry <henrym2@tcd.ie>
2020-07-28 16:03:35 +0100

    Implemented logging for Fetch Metadata requests Added logger mock for testing Decoupled logging from policies Directed logging to security channel

--- EventSubscriber/FetchMetadataRequestSubscriber.php
+++ EventSubscriber/FetchMetadataRequestSubscriber.php
@@ -4,7 +4,9 @@ namespace Ise\WebSecurityBundle\EventSubscriber;
 
 use Ise\WebSecurityBundle\Options\ConfigProviderInterface;
 use Ise\WebSecurityBundle\Policies\FetchMetadataPolicyProvider;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -13,10 +15,13 @@ class FetchMetadataRequestSubscriber implements EventSubscriberInterface
 {
     private $fetchMetadataPolicyProvider;
     private $configProvider;
-    public function __construct(FetchMetadataPolicyProvider $fetchMetadataPolicyProvider, ConfigProviderInterface $configProvider)
+    private $logger;
+
+    public function __construct(FetchMetadataPolicyProvider $fetchMetadataPolicyProvider, ConfigProviderInterface $configProvider, LoggerInterface $securityLogger)
     {
         $this->fetchMetadataPolicyProvider = $fetchMetadataPolicyProvider;
         $this->configProvider = $configProvider;
+        $this->logger = $securityLogger;
     }
     public static function getSubscribedEvents()
     {
@@ -32,14 +37,33 @@ class FetchMetadataRequestSubscriber implements EventSubscriberInterface
         $request = $event->getRequest();
         $options = $this->configProvider->getPathConfig($request);
         $fetchMetadataPolicy = $this->fetchMetadataPolicyProvider->getFetchMetadataPolicy($options['fetch_metadata']);
-
         if ($options['fetch_metadata']['active']) {
             $response = new Response();
             $response->headers->set('Vary', 'sec-fetch-site, sec-fetch-dest, sec-fetch-mode');
+
             if (!$fetchMetadataPolicy->applyPolicy($request)) {
+                $this->logRejection($request, $options['fetch_metadata']['policy']);
+
                 $response->setStatusCode(Response::HTTP_UNAUTHORIZED);
                 $event->setResponse($response);
             }
         }
+    }
+    /**
+     * Log rejection function. Logs if a request has been rejected by the fetch metadata policy.
+     * Records the Url, and which policy rejected the request.
+     *
+     * @param String $policy, A reference string to the Fetch Metadata policy used in the request. If Null assumed Default Policy
+     * @param Request $request, The request
+     * @return void
+     */
+    private function logRejection(Request $request, $policy): void
+    {
+        $debugContext = [
+            'request' => $request->getContent(),
+            'policy' => $policy
+        ];
+        $fetchMetaPolicy = $policy ?? 'Default Policy';
+        $this->logger->debug($request->getMethod().' request from host '.$request->getUri().' blocked by Fetch Metadata Policy: '.$fetchMetaPolicy, $debugContext);
     }
 }
