c2e90faa2cb481a7af423a49b973ca3149cc48c8
Matthew Henry <henrym2@tcd.ie>
2020-07-31 16:20:03 +0100

    Moved Config to class constant

--- DependencyInjection/IseWebSecurityExtension.php
+++ DependencyInjection/IseWebSecurityExtension.php
@@ -5,28 +5,27 @@ use Exception;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\Yaml\Yaml;
 
 class IseWebSecurityExtension extends Extension
 {
+    private const CONFIG_PATH = __DIR__.'/../Resources/config';
     public function load(array $configs, ContainerBuilder $container)
     {
         //!Work in progress as described in Configuration.php To be rebuild in #7 and #3
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-        
+
         $defaults = $config['defaults'];
         
         $loader = new YamlFileLoader(
             $container,
-            new FileLocator(__DIR__.'/../Resources/config')
+            new FileLocator(self::CONFIG_PATH)
         );
         $loader->load('services.yaml');
         
-        $presets = Yaml::parseFile(__DIR__.'/../Resources/config/presets.yaml')['presets'];
+        $presets = Yaml::parseFile(self::CONFIG_PATH.'/presets.yaml')['presets'];
         
         if (isset($defaults['preset'])) {
             $defaults = array_merge($defaults, $presets[$defaults['preset']]);
