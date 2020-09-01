ea3f7b036883e93c748d24f6b7b94fc7397f09d3
Matthew Henry <henrym2@tcd.ie>
2020-09-01 13:20:27 +0100

    Fixed minor issues Added is acrive check Added logging for policy conflicts Changed confusing var name

--- EventSubscriber/TrustedTypesSubscriber.php
+++ EventSubscriber/TrustedTypesSubscriber.php
@@ -3,6 +3,8 @@
 namespace Ise\WebSecurityBundle\EventSubscriber;
 
 use Ise\WebSecurityBundle\Options\ConfigProviderInterface;
+use Ise\WebSecurityBundle\Options\ContextChecker;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -14,10 +16,15 @@ use Symfony\Component\HttpKernel\KernelEvents;
 class TrustedTypesSubscriber implements EventSubscriberInterface
 {
     private $configProvider;
+    private $context;
+    private $logger;
+    private $policyIssueMessage = "Trusted types policy already defined in CSP header in request from %s. This may cause unexpected behaviour.";
 
-    public function __construct(ConfigProviderInterface $configProvider)
+    public function __construct(ConfigProviderInterface $configProvider, ContextChecker $context, LoggerInterface $logger)
     {
         $this->configProvider = $configProvider;
+        $this->context = $context;
+        $this->logger = $logger;
     }
 
     public static function getSubscribedEvents()
@@ -35,11 +42,21 @@ class TrustedTypesSubscriber implements EventSubscriberInterface
         $request = $event->getRequest();
 
         $options = $this->configProvider->getPathConfig($request);
-
+        //Check is trusted types is active, if not then leave handler
+        if (!$options['trusted_types']['active']) {
+            return;
+        }
         //Check if CSP header is set
+        $this->context->checkSecure($request, 'Trusted types');
         $headerSet = $response->headers->has("Content-Security-Policy");
         //If CSP header is set, pull it and append a ';' separator, else set an empty prefix.
         $headerPrefix = $headerSet ? $response->headers->get("Content-Security-Policy").';' : '';
+        //Check if trusted types policy is set. If so, print unexpected behaviour error
+        if (strpos($headerPrefix, 'trusted-types')) {
+            $policyIssue = sprintf($this->policyIssueMessage, $request->getUri());
+            $this->logger->log(0, $policyIssue, ['CSP header' => $headerPrefix]);
+        }
+        
         //Set trusted types CSP policy, and append it to the current policy if one exists
         $response->headers->set("Content-Security-Policy", $this->constructTrustedTypesHeader($options['trusted_types'], $headerPrefix));
     }
@@ -51,12 +68,12 @@ class TrustedTypesSubscriber implements EventSubscriberInterface
      * @param String $headerSet
      * @return String
      */
-    private function constructTrustedTypesHeader($options, $headerSet)
+    private function constructTrustedTypesHeader($options, $headerPrefix)
     {
         $policies = "trusted-types ".implode(" ", $options['policies']);
         $requireFor = "require-trusted-types-for ".implode(" ", array_map(function ($value) {
             return sprintf('\'%s\'', $value);
         }, $options['require_for']));
-        return sprintf("%s %s; %s;", $headerSet, $policies, $requireFor);
+        return sprintf("%s %s; %s;", $headerPrefix, $policies, $requireFor);
     }
 }
